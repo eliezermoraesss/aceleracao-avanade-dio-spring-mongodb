f6b373b2980d34cac3a5c0bab600e2e73451875e
eliezer <eliezer.moraes@outlook.com>
2023-05-06 10:31:45 -0300

    First commit - Project Created

---
+++ src/main/java/com/aceleracaoavanade/mentoriaavanade/MentoriaAvanadeApplication.java
@@ -0,0 +1,12 @@
+package com.aceleracaoavanade.mentoriaavanade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoriaAvanadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoriaAvanadeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/aceleracaoavanade/mentoriaavanade/MentoriaAvanadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aceleracaoavanade.mentoriaavanade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoriaAvanadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
